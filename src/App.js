fce24034eb10aded06dac3fb1e32e1fb39304537
Mitch <MitchelClark1997@outlook.com>
2023-08-30 10:11:58 +1000

    Added graphql and storage

--- src/App.js
+++ src/App.js
@@ -89,6 +89,12 @@ const App = ({ signOut }) => {
             variation="quiet"
             required
           />
+          <View
+            name="image"
+            as="input"
+            type="file"
+            style={{ alignSelf: "end" }}
+          />
           <Button type="submit" variation="primary">
             Create Note
           </Button>
@@ -120,7 +126,7 @@ const App = ({ signOut }) => {
           </Flex>
         ))}
       </View>
-      <View name="image" as="input" type="file" style={{ alignSelf: "end" }} />
+
       <Button onClick={signOut}>Sign Out</Button>
     </View>
   );
